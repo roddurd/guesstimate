7093366631790b9b4d45ddb68c37921499cd8f2d
Roddur Dasgupta <roddurd@gmail.com>
2019-07-29 21:13:58 -0700

    error proportional to magnitude, quit func

--- guesstimate.py
+++ guesstimate.py
@@ -1,4 +1,4 @@
-import timeit
+import timeit, math
 from random import randint, choice
 difficulty = input("Select difficulty: (E)asy (M)edium (H)ard\n")
 difficulty = difficulty[0].strip().lower()
@@ -9,7 +9,7 @@ score = 0
 
 print("Prepare to Guesstimate!")
 print("=========================")
-
+print("enter 'quit' or 'q' at any time to quit the game")
 
 	
 def multiply():
@@ -61,21 +61,24 @@ def exponentiate():
 	global difficulty
 	if difficulty == "e":
 		num1,num2 = randint(2,100), randint(2,100)
-		return ("What is " + str(num1) + " ^ " + str(num2) + "?", num1^num2)
+		return ("What is " + str(num1) + " ^ " + str(num2) + "?", num1**num2)
 	elif difficulty == "m":
 		num1,num2 = randint(8,10000), randint(8,10000)
-		return ("What is " + str(num1) + " ^ " + str(num2) + "?", num1^num2)
+		return ("What is " + str(num1) + " ^ " + str(num2) + "?", num1**num2)
 	elif difficulty == "h":
 		num1,num2 = randint(101,3000000), randint(108,3000000)
-		return ("What is " + str(num1) + " ^ " + str(num2) + "?", num1^num2)
+		return ("What is " + str(num1) + " ^ " + str(num2) + "?", num1**num2)
 
 
 
 		
 while True:
 	question, answer = choice((multiply(), divide(), add(), subtract(), exponentiate()))
-	guess = int(input(question+"\nYour answer: "))
-	score += abs(answer - guess)
+	guess = input(question+"\nYour answer: ")
+	if guess[0].lower() == 'q':
+		break
+	guess = int(guess)
+	score += abs(answer - guess) / 10**(math.log10(max((answer,guess)))-1)
 	print("The correct answer was:", answer)
 	print("You were off by ", abs(guess-answer))
 	print("Score: ", score,"\n")
